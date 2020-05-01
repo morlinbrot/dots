7842dbec4f346f4543a5bec3d7f71785c8ae7b21
synul <synul@mailbox.org>
2020-05-01 12:59:05 +0200

    Clean up rust setup

--- setup.rust.sh
+++ setup.rust.sh
@@ -8,20 +8,7 @@ fi
 # Install favourite components.
 rustup component add clippy rustfmt
 
-# Adds commands like 'cargo add' for installing crates.
-cargo install cargo-edit
-
-# Shows a handy overview over outdated dependencies.
-cargo install cargo-outdated
-
-# A modern ls replacement written in Rust.
-cargo install exa
-
-# Blazingly fast grep replacement.
-cargo install ripgrep
-
-# Install wasm-pack
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh 
+cargo install bat cargo-edit exa ripgrep wasm-pack
 
 # Install rust-analyzer.
 git clone https://github.com/rust-analyzer/rust-analyzer && cd rust-analyzer
