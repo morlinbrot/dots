f12a451c8306d5bef816794b64219fa9a22e8209
synul <synul@mailbox.org>
2020-05-25 23:12:02 +0200

    Move Caps Lock remapping to permanent templates/keyboard

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Re-map Caps Lock to Ctrl
-setxkbmap -option caps:ctrl_modifier
-
 # Create prx folder (some aliases will point to certain locations in here)
 mkdir -p ~/prx
 
