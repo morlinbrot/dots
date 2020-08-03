465aff39dbfceaa2c7841a6c06271387640cefce
synul <synul@mailbox.org>
2020-08-03 22:09:36 +0200

    Make tmus session setup more robust

--- tmux-session.sh
+++ tmux-session.sh
@@ -2,10 +2,30 @@
 
 # Start a tmux session with a default window and pane setup.
 
-tmux new-session \; \
-    send-keys 'cd ~/dots' Enter C-l \; \
-    rename-window tty \; \
-    new-window \; \
-    rename-window proj \; \
-    select-window -t tty \; \
+session="main"
+win1="tty"
+win2="proj"
+
+tmux has-session -t $session 2>/dev/null
+
+if [ $? != 0 ]
+then
+    tmux new-session -d -s $session
+fi
+
+tmux select-window -t $session:1;
+tmux rename-window $win1;
+tmux send-keys 'cd ~/dots' Enter C-l;
+
+tmux select-window -t $session:2 2>/dev/null;
+if [ $? != 0 ]
+then
+    tmux new-window;
+fi
+
+tmux rename-window $win2;
+tmux send-keys 'cd ~/prx' Enter C-l;
+tmux select-window -t $win1;
+
+tmux attach-session -t $session 
 
