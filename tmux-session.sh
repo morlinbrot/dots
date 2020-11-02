dacff76ce1108024dae4461f7519b054311d482e
synul <synul@mailbox.org>
2020-11-02 20:41:44 +0100

    Update default tmux setup

--- tmux-session.sh
+++ tmux-session.sh
@@ -11,20 +11,20 @@ tmux has-session -t $session 2>/dev/null
 if [ $? != 0 ]
 then
     tmux new-session -d -s $session
+    tmux send-keys 'cd ~/dots' Enter C-l;
 fi
 
 tmux select-window -t $session:1;
 tmux rename-window $win1;
-tmux send-keys 'cd ~/dots' Enter C-l;
 
 tmux select-window -t $session:2 2>/dev/null;
 if [ $? != 0 ]
 then
     tmux new-window;
+    tmux send-keys 'cd ~/prx' Enter C-l;
 fi
 
 tmux rename-window $win2;
-tmux send-keys 'cd ~/prx' Enter C-l;
 tmux select-window -t $win1;
 
 tmux attach-session -t $session 
