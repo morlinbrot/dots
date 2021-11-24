e20744e59c85c25e922aafb78376825f7fb2c358
synul <synul@mailbox.org>
2021-11-24 22:36:05 +0100

    Complete migration to lua + packer + lsp + nvim-cmp

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,49 +1,133 @@
+-- With inspiration from
+-- https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
 local lspconfig = require'lspconfig'
 local lspinstall = require'lspinstall'
 
 lspinstall.setup()
 
+-- -- -- -- -- -- -- --
+-- Setup nvim-cmp.
+-- -- -- -- -- -- -- --
+local cmp = require'cmp'
+local luasnip = require'luasnip'
+
+vim.o.completeopt = 'menuone,noselect'
+
+cmp.setup({
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            luasnip.lsp_expand(args.body) -- For `luasnip` users.
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        end,
+    },
+    mapping = {
+        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+        ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+        ['<C-e>'] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+        }),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        -- ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
+        -- ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+        ['<Tab>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end,
+        ['<S-Tab>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end,
+    },
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' }, -- For luasnip users.
+    }, {
+        { name = 'buffer' },
+    })
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    })
+})
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+-- -- -- -- -- -- -- --
+-- Setup lsp.
+-- -- -- -- -- -- -- --
 -- https://users.rust-lang.org/t/how-to-disable-rust-analyzer-proc-macro-warnings-in-neovim/53150/6
-local rust_config = {
-    settings = {
-        ["rust-analyzer"] = {
-            diagnostics = {
-                enable = true,
-                disabled = {"unresolved-proc-macro"},
-                enableexperimental = true,
-            }
+local rust_settings = {
+    ["rust-analyzer"] = {
+        diagnostics = {
+            enable = true,
+            disabled = {"unresolved-proc-macro"},
+            enableexperimental = true,
         }
     }
 }
 
-local lua_config = {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
+local lua_settings = {
+    Lua = {
+        diagnostics = {
+            globals = { 'vim' }
+        },
+        workspace = {
+            -- Make the server aware of Neovim runtime files
+            library = {
+                [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
             },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = {
-                    [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-                    [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-                },
-            },
-        }
+        },
     }
 }
+
+local on_attach = function(_, bufnr)
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+end
 
 local function setup_servers()
     local servers = lspinstall.installed_servers()
     for _, server in pairs(servers) do
-        local config = {}
+        local settings = {}
         if server == 'lua' then
-            config = lua_config
+            settings = lua_settings
         end
         if server == 'rust' then
-            config = rust_config
+            settings = rust_settings
         end
-        lspconfig[server].setup(config)
+        lspconfig[server].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = settings,
+        })
     end
 end
 
@@ -53,28 +137,3 @@ lspinstall.post_install_hook = function()
     setup_servers()
     vim.cmd('bufdo e')
 end
-
--- Who even uses C# anymore...
--- local pid = vim.fn.getpid()
--- -- On linux/darwin if using a release build, otherwise under scripts/OmniSharp(.Core)(.cmd)
--- local omnisharp_bin = "/opt/omnisharp/run"
--- -- on Windows
--- -- local omnisharp_bin = "/path/to/omnisharp/OmniSharp.exe"
--- lspconfig.omnisharp.setup {
---     cmd = { omnisharp_bin, "--languageserver" , "--hostPID", tostring(pid) };
--- }
-
--- local configs = require'lspconfig/configs'
--- local util = require'lspconfig/util'
--- local server_name = 'omnisharp'
-
--- configs[server_name] = {
---   default_config = {
---     filetypes = {"cs", "vb"};
---     root_dir = util.root_pattern("*.csproj", "*.sln");
---     init_options = {
---     };
---   };
---   -- on_new_config = function(new_config) end;
---   -- on_attach = function(client, bufnr) end;
--- }
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,19 +1,65 @@
 local cmd = vim.cmd
 local fn = vim.fn
-
--- Bootstrap packer using nvim's native package functionality.
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  cmd 'packadd packer.nvim'
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
--- The `use` param is only here to appease the linter. It is actually being
--- injected into the function's scope.
+-- Run :PackerCompile whenever plugins.lua gets updated.
+cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+cmd('silent! colorscheme githubsy')
+
 return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'            -- Packer can manage itself.
 
-    use 'wbthomason/packer.nvim'
+  use 'tmux-plugins/vim-tmux-focus-events'-- Activate focus events in emulators like tmux.
+  use 'itchyny/lightline.vim'             -- Displays a line that shows what mode you're in.
+  use 'tpope/vim-commentary'              -- Comment and un-comment with 'gc'.
+  use 'editorconfig/editorconfig-vim'     -- Support for .editorconfig files.
+  use 'chrisbra/Colorizer'                -- Colorise color strings, see <leader>cc keymap below.
+  use 'alvan/vim-closetag'                -- Auto-closing for xml tags.
+  use 'airblade/vim-rooter'               -- Moves the CL to the closest git repo root folder.
+  use { 'junegunn/fzf', run = './install --all' }       -- ...using a fuzzy finder to quickly find files in the same dir.
+  use 'junegunn/fzf.vim'
+  use 'sbdchd/neoformat'                  -- Auto-formatting for various languages.
+  use 'tpope/vim-surround'                -- Add "surround editing-- with "cs".
+  use 'vimwiki/vimwiki'
 
-    use 'tpope/vim-surround'        -- Add "surround editing" with "cs".
+  use 'neovim/nvim-lspconfig'
+  use { 'kabouzeid/nvim-lspinstall', branch = 'main' }  -- Add a :LspInstall command.
+  use 'tjdevries/lsp_extensions.nvim'
 
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+
+  -- Snippets.
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'L3MON4D3/LuaSnip'
+
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }  -- Treesitter, update parsers on update.
+  use 'nvim-treesitter/playground'
+
+  -- Themes
+  --use { 'dracula/vim',  as = 'dracula' }
+  --"use 'joshdick/onedark.vim'
+  --"use { 'humanoid-colors/vim-humanoid-colorscheme', 'branch': 'main' }
+  --"use 'arcticicestudio/nord-vim'
+  use 'wadackel/vim-dogrun'               -- Needed for lightline.
+  use { 'synul/githubsy.vim', branch = 'main' }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
+
