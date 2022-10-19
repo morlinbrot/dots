0514dfe33de7fee1a8631c7c1206d10fe1483c7c
morlinbrot <morlinbrot@mailbox.org>
2022-10-19 17:25:36 +0200

    Move unused stuff to archive folder

---
+++ archive/avim.init.lua
@@ -0,0 +1,278 @@
+--              AstroNvim Configuration Table
+-- All configuration changes should go inside of the table below
+
+-- You can think of a Lua "table" as a dictionary like data structure the
+-- normal format is "key = value". These also handle array like data structures
+-- where a value with no key simply has an implicit numeric key
+--
+-- Note to self: Clone astronvim repo to ~/.config/nvim, mkdir ~/.config/nvim/lua/user and then link to this file with
+-- ln -s ~/dots/avim.init.lua ~/.config/nvim/lua/user/init.lua
+local config = {
+
+	-- Configure AstroNvim updates
+	updater = {
+		remote = "origin", -- remote to use
+		channel = "nightly", -- "stable" or "nightly"
+		version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+		branch = "main", -- branch name (NIGHTLY ONLY)
+		commit = nil, -- commit hash (NIGHTLY ONLY)
+		pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
+		skip_prompts = false, -- skip prompts about breaking changes
+		show_changelog = true, -- show the changelog after performing an update
+		auto_reload = false, -- automatically reload and sync packer after a successful update
+		auto_quit = false, -- automatically quit the current session after a successful update
+		-- remotes = { -- easily add new remotes to track
+		--   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
+		--   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
+		--   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
+		-- },
+	},
+
+	colorscheme = "catppuccin",
+
+	-- set vim options here (vim.<first_key>.<second_key> =  value)
+	options = {
+		opt = {
+			guifont = "RobotoMono Nerd Font",
+		},
+		g = {},
+	},
+	-- If you need more control, you can use the function()...end notation
+	-- options = function(local_vim)
+	--   local_vim.opt.relativenumber = true
+	--   local_vim.g.mapleader = " "
+	--   local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
+	--   local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
+	--
+	--   return local_vim
+	-- end,
+
+	-- Mapping data with "desc" stored directly by vim.keymap.set().
+	--
+	-- Please use this mappings table to set keyboard mapping since this is the
+	-- lower level configuration and more robust one. (which-key will
+	-- automatically pick-up stored data by this setting.)
+	mappings = {
+		-- first key is the mode
+		n = {
+			-- second key is the lefthand side of the map
+			-- mappings seen under group name "Buffer"
+			["<leader>bb"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+			["<leader>bc"] = { "<cmd>BufferLinePickClose<cr>", desc = "Pick to close" },
+			["<leader>bj"] = { "<cmd>BufferLinePick<cr>", desc = "Pick to jump" },
+			["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = "Sort by tabs" },
+			-- quick save
+			-- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+			["gl"] = { "$", desc = "End of line ($)" },
+			["gh"] = { "0", desc = "Beginning of line (0)" },
+			["gs"] = { "_", desc = "First non-whitespace of line (_)" },
+			["ge"] = { "G", desc = "End of file (G)" },
+			["U"] = { "<C-r>", desc = "Redo" },
+		},
+		t = {
+			-- setting a mapping to false will disable it
+			-- ["<esc>"] = false,
+		},
+		i = {
+			["<C-j>"] = { "<esc>" },
+		},
+	},
+
+	-- Configure plugins
+	plugins = {
+		init = {
+			{
+				"catppuccin/nvim",
+				as = "catppuccin",
+				config = function()
+					vim.g.catppuccin_flavour = "mocha"
+					require("catppuccin").setup()
+				end,
+			},
+			{ "windwp/nvim-spectre", requires = "nvim-lua/plenary.nvim" },
+			{ "morlinbrot/githubsy.vim", as = "githubsy", branch = "main" },
+		},
+		-- All other entries override the require("<key>").setup({...}) call for default plugins
+		["null-ls"] = function(config) -- overrides `require("null-ls").setup(config)`
+			-- config variable is the default configuration table for the setup functino call
+			-- local null_ls = require("null-ls")
+			-- Check supported formatters and linters
+			-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+			-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+			config.sources = {
+				-- Set a formatter
+				-- null_ls.builtins.formatting.stylua,
+				-- null_ls.builtins.formatting.prettier,
+			}
+			return config -- return final config table to use in require("null-ls").setup(config)
+		end,
+		treesitter = { -- overrides `require("treesitter").setup(...)`
+			ensure_installed = { "lua" },
+		},
+		-- use mason-lspconfig to configure LSP installations
+		["mason-lspconfig"] = { -- overrides `require("mason-lspconfig").setup(...)`
+			ensure_installed = { "sumneko_lua" },
+		},
+		packer = { -- overrides `require("packer").setup(...)`
+			compile_path = vim.fn.stdpath("data") .. "/packer_compiled.lua",
+		},
+		["declancm/cinnamon.nvim"] = {
+			config = function()
+				require("cinnamon").setup()
+			end,
+		},
+	},
+
+	-- LuaSnip Options
+	luasnip = {
+		-- Add paths for including more VS Code style snippets in luasnip
+		vscode_snippet_paths = {},
+		-- Extend filetypes
+		filetype_extend = {
+			javascript = { "javascriptreact" },
+		},
+	},
+
+	-- CMP Source Priorities
+	-- modify here the priorities of default cmp sources
+	-- higher value == higher priority
+	-- The value can also be set to a boolean for disabling default sources:
+	-- false == disabled
+	-- true == 1000
+	cmp = {
+		source_priority = {
+			nvim_lsp = 1000,
+			luasnip = 750,
+			buffer = 500,
+			path = 250,
+		},
+		-- TODO: Find out how to do this.
+		mapping = {
+			["<C-j>"] = nil,
+		},
+	},
+
+	-- Modify which-key registration (Use this with mappings table in the above.)
+	["which-key"] = {
+		-- Add bindings which show up as group name
+		register = {
+			-- first key is the mode, n == normal mode
+			n = {
+				-- second key is the prefix, <leader> prefixes
+				["<leader>"] = {
+					-- third key is the key to bring up next level and its displayed
+					-- group name in which-key top level menu
+					["b"] = { name = "Buffer" },
+				},
+			},
+		},
+	},
+
+	-- Override highlight groups in any theme
+	highlights = {
+		-- duskfox = { -- a table of overrides/changes to the default
+		--   Normal = { bg = "#000000" },
+		-- },
+		default_theme = function(highlights) -- or a function that returns a new table of colors to set
+			local C = require("default_theme.colors")
+
+			highlights.Normal = { fg = C.fg, bg = C.bg }
+			return highlights
+		end,
+	},
+
+	-- Default theme configuration
+	default_theme = {
+		-- set the highlight style for diagnostic messages
+		diagnostics_style = { italic = true },
+		-- Modify the color palette for the default theme
+		colors = {
+			fg = "#abb2bf",
+			bg = "#1e222a",
+		},
+		-- enable or disable highlighting for extra plugins
+		plugins = {
+			aerial = true,
+			beacon = false,
+			bufferline = true,
+			dashboard = true,
+			highlighturl = true,
+			hop = false,
+			indent_blankline = true,
+			lightspeed = false,
+			["neo-tree"] = true,
+			notify = true,
+			["nvim-tree"] = false,
+			["nvim-web-devicons"] = true,
+			rainbow = true,
+			symbols_outline = false,
+			telescope = true,
+			vimwiki = false,
+			["which-key"] = true,
+		},
+	},
+
+	-- Diagnostics configuration (for vim.diagnostics.config({...}))
+	diagnostics = {
+		virtual_text = true,
+		underline = true,
+	},
+
+	-- Extend LSP configuration
+	lsp = {
+		-- enable servers that you already have installed without mason
+		servers = {
+			-- "pyright"
+		},
+		formatting = {
+			format_on_save = true,
+		},
+		-- easily add or disable built in mappings added during LSP attaching
+		mappings = {
+			n = {
+				-- ["<leader>lf"] = false -- disable formatting keymap
+				["gl"] = false, -- Make room for "End of line" above.
+			},
+		},
+	},
+
+	-- This function is run last and is a good place to configuring
+	-- augroups/autocommands and custom filetypes also this just pure lua so
+	-- anything that doesn't fit in the normal config locations above can go here
+	polish = function()
+		--vim.api.nvim_create_augroup("highlight_yank", { clear = true })
+		vim.api.nvim_create_autocmd("TextYankPost", {
+			--vim.api.nvim_create_augroup("highlight_yank"),
+			desc = "Highlight yanked text shortly",
+			--group = "highlight_yank",
+			pattern = "*",
+			callback = function()
+				vim.highlight.on_yank({ higroup = "IncSearch", timeout = 300 })
+			end,
+		})
+		-- Set key binding
+		-- Set autocommands
+		vim.api.nvim_create_augroup("packer_conf", { clear = true })
+		vim.api.nvim_create_autocmd("BufWritePost", {
+			desc = "Sync packer after modifying plugins.lua",
+			group = "packer_conf",
+			pattern = "plugins.lua",
+			command = "source <afile> | PackerSync",
+		})
+
+		-- Set up custom filetypes
+		-- vim.filetype.add {
+		--   extension = {
+		--     foo = "fooscript",
+		--   },
+		--   filename = {
+		--     ["Foofile"] = "fooscript",
+		--   },
+		--   pattern = {
+		--     ["~/%.config/foo/.*"] = "fooscript",
+		--   },
+		-- }
+	end,
+}
+
+return config
